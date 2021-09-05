451facce347864fa039cb9448417079fd8f1591d
yuta.nishio <yuta.nishio@uzabase.com>
2021-09-05 22:24:02 +0900

    todo-yuta-web:ユーザーは作成画面で詳細を入力することができる

--- todo-yuta-web/src/components/create_task.jsx
+++ todo-yuta-web/src/components/create_task.jsx
@@ -6,6 +6,7 @@ import './create_task.scss';
 
 function CreateTask(props){
     const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('')
 
     return (
         <div className='create-task'>
@@ -15,7 +16,10 @@ function CreateTask(props){
                     <p className='label'>Title</p>
                     <input　className='input-title' type="text" value={title} onChange={(e) =>  setTitle(e.target.value)} />
                 </div>
-
+                <div className='description'>
+                    <p className='label'>Description</p>
+                    <textarea  className='input-description' value={description} onChange={(e) => setDescription(e.target.value)} cols="30" rows="10"></textarea>
+                </div>
             </div>
             <BackTopButton onClick={() => props.onClick()}></BackTopButton>
 
