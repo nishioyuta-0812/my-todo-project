a22279f354ae3273ec095d423cce7f27ca9cba7f
yuta.nishio <yuta.nishio@uzabase.com>
2021-10-17 15:09:03 +0900

    todo-yuta-api: line bot用のAPIの口を作成

--- todo-yuta-api/api/controllers/taskController.js
+++ todo-yuta-api/api/controllers/taskController.js
@@ -1,35 +1,58 @@
 const db = require("../../models/index");
+const line = require('@line/bot-sdk');
 
-exports.create_task = function(req, res) {
-    console.log(req.body)
-    db.task.create({
-        title: req.body.task.title,
-        task_descrption: req.body.task.description
-      }).then(() => {
-        res.send("Data Created.");
-    });
+const config = {
+  channelSecret: '83527eb0f792b906f54d19320e24b5bd',
+  channelAccessToken: 'Osb56U39J6CGAUhAuHirof+APu9FlXJsxrFFG2Astn+twZpLiE0jz1SWAkt6xfIRTnStYdIxVye/34DPYUw8Z9gDxUUyddnTFn7CF1umCqXI/XLLmXb46+Bih5/ONiRzxjy190v7cpl2dPfqjUD2UwdB04t89/1O/w1cDnyilFU='
+};
+const client = new line.Client(config);
+
+exports.create_task = function (req, res) {
+  console.log(req.body)
+  db.task.create({
+    title: req.body.task.title,
+    task_descrption: req.body.task.description
+  }).then(() => {
+    res.send("Data Created.");
+  });
 };
 
 
-exports.get_tasks = function(req, res) {
-    db.task.findAll()
+exports.get_tasks = function (req, res) {
+  db.task.findAll()
     .then((tasks) => {
-        res.send({tasks: tasks});
+      res.send({ tasks: tasks });
     });
 };
 
-exports.delete_task = function(req, res) {
-    db.task.destroy({
-        where: {
-            id: [req.params.taskId]
-        }
-    }).then(() => {
-        res.send("Data Deleted.");
-    });
+exports.delete_task = function (req, res) {
+  db.task.destroy({
+    where: {
+      id: [req.params.taskId]
+    }
+  }).then(() => {
+    res.send("Data Deleted.");
+  });
 };
 
-exports.reset_task = function(req, res) {
-    db.task.destroy({ truncate : true, cascade: false }).then(() => {
-        res.send("Data Reseted.");
-    });
+exports.reset_task = function (req, res) {
+  db.task.destroy({ truncate: true, cascade: false }).then(() => {
+    res.send("Data Reseted.");
+  });
 };
+
+exports.check_notification = async (req, res) => {
+  const messages = [{
+    type: 'text',
+    text: '今日のタスクがまだ残ってるよ！'
+  }];
+
+  try {
+    const re = await client.pushMessage('U615d5fec7a2484a239548014acfac04c',messages);
+    console.log(re);
+    res.send('success');
+  } catch (error) {
+    console.log(`エラー: ${error.statusMessage}`);
+    console.log(error.originalError.response.data);
+  }
+}
--- todo-yuta-api/api/routes/taskRoutes.js
+++ todo-yuta-api/api/routes/taskRoutes.js
@@ -1,17 +1,19 @@
-module.exports = function(app) {
-    var taskList = require('../controllers/taskController');
+module.exports = function (app) {
+  var taskList = require('../controllers/taskController');
 
-    app.route('/tasks')
-    .get(taskList.get_tasks)
+  app.route('/tasks')
+    .get(taskList.get_tasks);
 
-    app.route('/create')
+  app.route('/create')
     .post(taskList.create_task);
-    app.route('/delete/:taskId')
+  app.route('/delete/:taskId')
     .delete(taskList.delete_task);
 
-    app.route('/update/:taskId')
+  app.route('/update/:taskId')
 
-    app.route('/reset')
+  app.route('/reset')
     .delete(taskList.reset_task);
-    
+
+  app.route('/check/tasks')
+    .get(taskList.check_notification);
 };
